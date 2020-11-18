43512139c450c4d50c953f8ac5811bd059c10837
khanhhunghoccode <daokhanhhunghm9.@gmail.com>
2020-11-18 10:26:18 +0700

    add recursion and still see GREEN

--- src/hung/util/MathUtility.java
+++ src/hung/util/MathUtility.java
@@ -25,19 +25,30 @@ public class MathUtility {
     //bài này sẽ chặn giai thừa từ 0..20
     //âm hoặc 21 trở lên không tính
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20) {
-            throw new IllegalArgumentException("n must be between 0...20");
-        }
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        //viết else vô nghĩa, vì có return
-        //mặc nhiên CPU đến được đây là hiểu rằng n=2...20
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("n must be between 0...20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        //viết else vô nghĩa, vì có return
+//        //mặc nhiên CPU đến được đây là hiểu rằng n=2...20
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+          //dùng đệ quy để gọi lại với quy mô nhỏ hơn
+          //5! = 5 * 4!; 4! = 4 * 3!; 3! = 3 * 2!...
+          //kết luận: n! = n * (n-1)!
+          //phải có điểm dừng để tránh vòng lặp vô tận
+          if (n < 0 || n > 20) {
+              throw new IllegalArgumentException("n must be between 0...20");
+          }
+          if (n == 0 || n == 1) {
+              return 1;//điều kiện dừng
+          }
+          return n * getFactorial(n - 1);//đệ quy
     }
 
 }
